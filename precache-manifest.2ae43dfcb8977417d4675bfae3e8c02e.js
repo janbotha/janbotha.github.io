65675248511fdd08d29cfa65b1f4f19aaf9db950
janbotha <janhbotha@yahoo.com.au>
2019-06-10 11:41:33 +0100

    New Template

---
+++ precache-manifest.2ae43dfcb8977417d4675bfae3e8c02e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3376434e2087b8723b6",
+    "url": "/my-retail/static/js/runtime~main.b3376434.js"
+  },
+  {
+    "revision": "0caf7e44f22da8cbbb22",
+    "url": "/my-retail/static/js/main.0caf7e44.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/my-retail/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "0caf7e44f22da8cbbb22",
+    "url": "/my-retail/static/css/main.7d65ecaa.chunk.css"
+  },
+  {
+    "revision": "c1105c41a70dcfb37f9df41d8d345582",
+    "url": "/my-retail/index.html"
+  }
+];
